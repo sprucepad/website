7fe1ffa8f7ed913ab1827a16e4f655f5fff7cf9f
sprucepad <sprucepadhq@gmail.com>
2026-01-30 16:37:15 -0300

    feat: add project page in pt, add getCollectionWithImage helper

---
+++ src/components/Cards.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import type { CollectionEntryWithImage } from "@/lib/collection-w-image";
+
+  interface Props {
+    cards: CollectionEntryWithImage<"posts" | "projects">[];
+    topics: CollectionEntryWithImage<"topics">[];
+  }
+
+  const { cards, topics }: Props = $props();
+</script>
---
+++ src/content.config.ts
@@ -0,0 +1,59 @@
+import { file, glob } from "astro/loaders";
+import { defineCollection, reference, z } from "astro:content";
+
+const art = defineCollection({
+  loader: glob({ pattern: "**/*.{yml,yaml,json,toml}", base: "./content/art" }),
+  schema: ({ image }) =>
+    z.object({
+      cover: z.string(),
+      images: z.array(
+        z.object({
+          img: image(),
+          alt: z.string(),
+          license: z.string().default("All Rights Reserved"),
+        }),
+      ),
+    }),
+});
+
+const topics = defineCollection({
+  loader: file("./content/topics.yml"),
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      icon: image(),
+    }),
+});
+
+const posts = defineCollection({
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "./content/posts" }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      desc: z.string(),
+      image: image().optional(),
+      topics: z.array(reference("topics")).default([]),
+      created: z.date(),
+      updated: z.date(),
+      license: z.string().default("All Rights Reserved"),
+    }),
+});
+
+const projects = defineCollection({
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "./content/projects" }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      desc: z.string(),
+      image: image().optional(),
+      topics: z.array(reference("topics")).default([]),
+      created: z.date(),
+      updated: z.date(),
+
+      license: z.string().default("All Rights Reserved"),
+      github: z.string().optional(),
+      itchio: z.string().optional(),
+    }),
+});
+
+export const collections = { art, topics, posts, projects };
