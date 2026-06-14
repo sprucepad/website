e56fbbe1c16678de923c1863e835775983ac551e
sprucepad <sprucepadhq@gmail.com>
2026-06-14 13:53:39 -0300

    feat: add split-view styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 import vercel from "@astrojs/vercel";
 
@@ -18,6 +18,43 @@ export default defineConfig({
   adapter: vercel(),
   integrations: [sitemap(), mdx(), svelte()],
 
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": "/src",
+        "@content": "/content",
+      },
+    },
+  },
+
+  fonts: [
+    {
+      cssVariable: "--font-dm-sans",
+      provider: fontProviders.google(),
+      name: "DM Sans",
+    },
+    {
+      cssVariable: "--font-dm-mono",
+      provider: fontProviders.google(),
+      name: "DM Mono",
+    },
+    {
+      cssVariable: "--font-peaberry",
+      name: "Peaberry",
+      provider: fontProviders.local(),
+      options: {
+        variants: [
+          {
+            src: ["./content/fonts/Peaberry.ttf"],
+            weight: "variable",
+            style: "normal",
+          },
+        ],
+      },
+    },
+  ],
+
   i18n: {
     defaultLocale: "pt",
     locales: ["en", "pt"],
@@ -34,15 +71,5 @@ export default defineConfig({
         [rehypeExternalLinks, { target: "_blank", rel: "noopener noreferrer" }],
       ],
     }),
-  },
-
-  vite: {
-    plugins: [tailwindcss()],
-    resolve: {
-      alias: {
-        "@": "/src",
-        "@content": "/content",
-      },
-    },
   },
 });
